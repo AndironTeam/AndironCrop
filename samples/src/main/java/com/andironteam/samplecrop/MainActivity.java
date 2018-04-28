e1554ffbc7eacbdf3a04190b09cbe74ed1de8718
Andiron Dev <andiron.dev@gmail.com>
2018-04-28 13:53:31 +0430

    Init Sample Project

---
+++ samples/src/main/java/com/andironteam/samplecrop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.andironteam.samplecrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
